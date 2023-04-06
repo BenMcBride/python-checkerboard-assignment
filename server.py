468eef1183110ff6f55b5296b53e3239def3b07d
Benjamin <themonsterhidingunderyourbed@gmail.com>
2023-04-06 14:43:58 -0700

    updated the custom colors code

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-import math
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html", rows = 8, columns = 8   )
+    return render_template("index.html", rows = 8, columns = 8, darkClr = 'black', lightClr = 'red'  )
 
 @app.route('/<x>')
 def x(x):
@@ -13,6 +12,11 @@ def x(x):
 @app.route('/<x>/<y>')
 def xY(x, y):
     return render_template("index.html", rows = int(x), columns = int(y))
+
+@app.route('/<x>/<y>/<darkClr>/<lightClr>')
+def xYClrs(x, y, darkClr, lightClr):
+    return render_template("index.html", rows = int(x), columns = int(y), darkClr = darkClr, lightClr = lightClr)
+
 
 if __name__=="__main__":
     app.run(debug=True)
